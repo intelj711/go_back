772d2f5a4709bbff48d9bbc9d36745ec565f8165
FayeZ <im5dii@gmail.com>
2018-02-07 20:17:13 -0800

    change GCE URL

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ const (
 	PROJECT_ID = "protean-depot-194222"
 	BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.185.78.227:9200/"
+	ES_URL = "http://35.231.119.47:9200/"
 )
 
 func main() {
